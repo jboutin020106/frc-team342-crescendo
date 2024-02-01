32943b9684b668e2cde16324e5c9e7f8b0d2489d
unknown <jacobboutin36@gmail.com>
2024-02-01 18:53:27 -0500

    worked on connection check implementation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,12 +10,17 @@ import frc.robot.commands.Autos;
 import frc.robot.commands.Drive.DriveWithJoystick;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.SwerveDrive;
+
+import org.opencv.dnn.Net;
+
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -43,6 +48,8 @@ public class RobotContainer {
   private DriveWithJoystick driveWithJoystick;
   private JoystickButton toggleFieldOrientedBtn;
   private JoystickButton toggleDriveWithTargetingBtn;
+
+  private final NetworkTable hardware = NetworkTableInstance.getDefault().getTable("Hardware");
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -86,5 +93,17 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
     return null;
+  }
+
+  private Command checkHardwareCommand(){
+
+    return Commands.sequence(
+
+      new InstantCommand(
+          () -> {hardware.getEntry("Drive System").setString(swerve.hardwareConnections()); },
+          swerve
+      ),
+    
+    );
   }
 }
